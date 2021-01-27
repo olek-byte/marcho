825ea1aee759ab5dd27c3cd2f4a0f59c8aea1c85
olek-byte <olekhyzhniak@gmail.com>
2021-01-27 19:58:25 +0200

    check

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
 
-const scss         = require('gulp-sass');
-const concat       = require('gulp-concat');
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
-const uglify       = require('gulp-uglify');
-const imagemin     = require('gulp-imagemin');
-const rename     = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
 const nunjucksRender = require('gulp-nunjucks-render');
-const del          = require('del');
-const browserSync  = require('browser-sync').create();
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 function browsersync() {
   browserSync.init({
@@ -19,67 +25,81 @@ function browsersync() {
   })
 }
 
-function nunjucks () {
-  return src ('app/*.njk')
-  .pipe(nunjucksRender())
-  .pipe(dest('app'))
-  .pipe(browserSync.stream())
+function nunjucks() {
+  return src('app/*.njk')
+    .pipe(nunjucksRender())
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
 }
 
 function styles() {
   return src('app/scss/*.scss')
-  .pipe(scss({outputStyle: 'compressed'}))
-  // .pipe(concat())
-  .pipe(rename({
-    suffix : '.min'
-  }))
-  .pipe(autoprefixer({
-    overrideBrowserslist: ['last 10 versions'],
-    grid: true
-  }))
-  .pipe(dest('app/css'))
-  .pipe(browserSync.stream())
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
+    // .pipe(concat())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-    'node_modules/rateyo/src/jquery.rateyo.js',
-    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-    'app/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+      'app/js/main.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
 function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imagemin.mozjpeg({quality: 75, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-    imagemin.svgo({
-        plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
         ]
-    })
-]))
-  .pipe(dest('dist/images'))
+      })
+    ]))
+    .pipe(dest('dist/images'))
 }
 
 function build() {
   return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+      'app/**/*.html',
+      'app/css/style.min.css',
+      'app/js/main.min.js'
+    ], {
+      base: 'app'
+    })
+    .pipe(dest('dist'))
 }
 
 function cleanDist() {
@@ -116,9 +136,10 @@ exports.default = parallel(nunjucks, styles, scripts, browsersync, watching);
 // var gulp = require('gulp');
 // var browserSync = require('browser-sync').create();
 
-// gulp.task('sync', function() {
-//  browserSync.init({
-//  proxy: "my_project.dev",
-//  files: "*.css,*.php,css/*css"
-// });
+// gulp.task('sync', function () {
+//   browserSync.init({
+//     proxy: "my_project.dev",
+//     files: "*.css,*.php,css/*css"
+//   });
+
 // });
